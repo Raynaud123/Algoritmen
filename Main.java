6c7f1151258b317e9f25a3303e25237b96a8897c
Marieke Beke <marieke.beke@student.kuleuven.be>
2022-11-09 13:54:42 +0100

    Klasse kraan hoofdletter gegeven

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ import java.util.ArrayList;
 
 public class Main {
 
+    public static void main(String args[]) {
+        Kraan kraan1 = new Kraan();
+    }
 
 //methodes is er conflict, zo ja conflict pas de het pad aan om conflict te vermijden
 
