753f3f3a6d602eebe4f432a5d95b5c052b7be331
raynaud <raynaud.cornille@gmail.com>
2022-11-09 14:16:23 +0100

    Json file geadd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,19 @@
 package src.main.java;
 
+import org.json.simple.parser.JSONParser;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main{
 
-// Tegen volgend labo git aangemaakt + mail met groep en git repository
 //methodes is er conflict, zo ja conflict pas de het pad aan om conflict te vermijden
     public static void main(String[] args) {
+
+        JSONParser jsonParser = new JSONParser();
+
+
+
         Scanner sc = new Scanner(System.in);
 
         int aantalKranen = sc.nextInt();
